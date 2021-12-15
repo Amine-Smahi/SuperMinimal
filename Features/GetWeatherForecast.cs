75815d7cc8b87e61a7fc8d339200ca12ce8fe535
Amine Smahi <mohammed.amine.smahi@gmail.com>
2021-12-15 21:38:29 +0100

    Add project files

---
+++ Features/GetWeatherForecast.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Features;
+using Ardalis.ApiEndpoints;
+
+public class GetWeatherForecast : EndpointBaseAsync.WithoutRequest.WithResult<WeatherForecast[]>
+{
+    [HttpPost("api/GetWeatherForecast")]
+    public override Task<WeatherForecast[]> HandleAsync(CancellationToken cancellationToken = new())
+    {
+        string[] summaries = {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        }; 
+        
+        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = summaries[Random.Shared.Next(summaries.Length)]
+            })
+            .ToArray());
+    }
+}
